6a9921fbff27cfb39b1b558127e017596280cd36
Crowdbotics <team@crowdbotics.com>
2020-11-09 12:37:22 +0000

    Update components

--- src/features/BlankScreen114175/screens/blank.js
+++ src/features/BlankScreen114175/screens/blank.js
@@ -23,31 +23,30 @@ export default class Blank extends React.Component {
     }
   }
 
-  state = { TextInput_2: "", TextInput_3: "" }
+  state = { TextInput_3: "", TextInput_2: "" }
 
   render = () => (
     <View>
       <TextInput
-        placeholder="Sample text input placeholder"
-        value={this.state.TextInput_2}
-        onChangeText={nextValue => this.setState({ TextInput_2: nextValue })}
-      />
-      <TextInput
         placeholder="Number Input Placeholder"
-        keyboardType="numeric"
         style={styles.TextInput_3}
         value={this.state.TextInput_3}
         onChangeText={nextValue => this.setState({ TextInput_3: nextValue })}
       />
-      <Text style={styles.Text_4}>
-        Sample text content fgsvrfvc rtfgfvcx gtrefc fdvsc vfdcx rfvdcx fdc
-        rgvfcx tbgvf jmukjnhfgbfv fgbvc etgbfvdc trgfvdcsx
-      </Text>
       <Button
         title="Press me!"
         style={styles.Button_5}
         onPress={() => alert("Pressed!")}
       />
+      <TextInput
+        placeholder="Sample text input placeholder"
+        value={this.state.TextInput_2}
+        onChangeText={nextValue => this.setState({ TextInput_2: nextValue })}
+      />
+      <Text style={styles.Text_4}>
+        Sample text content fgsvrfvc rtfgfvcx gtrefc fdvsc vfdcx rfvdcx fdc
+        rgvfcx tbgvf jmukjnhfgbfv fgbvc etgbfvdc trgfvdcsx
+      </Text>
     </View>
   )
 }
@@ -57,9 +56,15 @@ const styles = StyleSheet.create({
     flex: 1,
     marginHorizontal: 16
   },
+
   View_1: {},
   TextInput_2: {},
   TextInput_3: { height: 100, alignSelf: "center" },
   Text_4: { alignSelf: "center" },
-  Button_5: { fontSize: 16, fontFamily: "RobotoMono-BoldItalic" }
+  Button_5: { fontSize: 16, fontFamily: "RobotoMono-BoldItalic" },
+  View_1: {},
+  TextInput_3: { height: 100, alignSelf: "center" },
+  Button_5: { fontSize: 16, fontFamily: "RobotoMono-BoldItalic" },
+  TextInput_2: {},
+  Text_4: { alignSelf: "center" }
 })
