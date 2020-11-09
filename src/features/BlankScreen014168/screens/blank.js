9cf3be78bd62832d5c4f446af8e210f5d475c3c9
Crowdbotics <team@crowdbotics.com>
2020-11-09 11:55:40 +0000

    Update components

--- src/features/BlankScreen014168/screens/blank.js
+++ src/features/BlankScreen014168/screens/blank.js
@@ -23,41 +23,9 @@ export default class Blank extends React.Component {
     }
   }
 
-  state = { TextInput_17: "", Switch_31: true }
+  state = {}
 
-  render = () => (
-    <View>
-      <View style={styles.View_2}>
-        <View style={styles.View_3}>
-          <Text>Sample text content</Text>
-          <TextInput
-            placeholder="Sample text input placeholder"
-            value={this.state.TextInput_17}
-            onChangeText={nextValue =>
-              this.setState({ TextInput_17: nextValue })
-            }
-          />
-          <Button title="Press me!" onPress={() => alert("Pressed!")} />
-          <Switch
-            style={styles.Switch_31}
-            value={this.state.Switch_31}
-            onValueChange={nextChecked =>
-              this.setState({ Switch_31: nextChecked })
-            }
-          />
-        </View>
-        <View />
-        <View />
-        <View />
-        <View />
-        <View style={styles.View_11} />
-      </View>
-      <View>
-        <View />
-        <View />
-      </View>
-    </View>
-  )
+  render = () => <View />
 }
 
 const styles = StyleSheet.create({
@@ -91,5 +59,6 @@ const styles = StyleSheet.create({
   View_11: { width: 100, height: 100 },
   View_5: {},
   View_6: {},
-  View_7: {}
+  View_7: {},
+  View_1: {}
 })
