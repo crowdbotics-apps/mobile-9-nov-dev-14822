f7049cde47a1233cebbda60d6f590be5d98933fe
Crowdbotics <team@crowdbotics.com>
2020-11-09 11:01:03 +0000

    Update components

--- src/features/BlankScreen314156/screens/blank.js
+++ src/features/BlankScreen314156/screens/blank.js
@@ -23,9 +23,50 @@ export default class Blank extends React.Component {
     }
   }
 
-  state = {}
+  state = {
+    TextInput_4: "",
+    TextInput_5: "",
+    Switch_6: true,
+    CheckBox_7: true,
+    CheckBox_8: true
+  }
 
-  render = () => <View />
+  render = () => (
+    <View>
+      <Text>Sample text content</Text>
+      <Button title="Press me!" onPress={() => alert("Pressed!")} />
+      <TextInput
+        placeholder="Number Input Placeholder"
+        keyboardType="numeric"
+        value={this.state.TextInput_4}
+        onChangeText={nextValue => this.setState({ TextInput_4: nextValue })}
+      />
+      <TextInput
+        placeholder="Sample text input placeholder"
+        value={this.state.TextInput_5}
+        onChangeText={nextValue => this.setState({ TextInput_5: nextValue })}
+      />
+      <Switch
+        trackColor={{ false: "#C0CCDA", true: "#409EFF" }}
+        style={styles.Switch_6}
+        value={this.state.Switch_6}
+        onValueChange={nextChecked => this.setState({ Switch_6: nextChecked })}
+      />
+      <CheckBox
+        title="Checkbox"
+        checked={this.state.CheckBox_7}
+        onPress={nextChecked => this.setState({ CheckBox_7: nextChecked })}
+      />
+      <CheckBox
+        title="Radio button"
+        checkedIcon="dot-circle-o"
+        uncheckedIcon="circle-o"
+        checked={this.state.CheckBox_8}
+        onPress={nextChecked => this.setState({ CheckBox_8: nextChecked })}
+      />
+      <Icon name="star" />
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -43,5 +84,14 @@ const styles = StyleSheet.create({
   CheckBox_7: {},
   Icon_8: {},
   CheckBox_9: {},
-  View_1: {}
+  View_1: {},
+  View_1: {},
+  Text_2: {},
+  Button_3: {},
+  TextInput_4: {},
+  TextInput_5: {},
+  Switch_6: { alignSelf: "flex-start" },
+  CheckBox_7: {},
+  CheckBox_8: {},
+  Icon_9: {}
 })
