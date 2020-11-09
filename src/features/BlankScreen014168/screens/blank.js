e7a61ace73d0cdecdbe49ecada6479c9ecf3a3f0
Crowdbotics <team@crowdbotics.com>
2020-11-09 11:53:06 +0000

    Update components

--- src/features/BlankScreen014168/screens/blank.js
+++ src/features/BlankScreen014168/screens/blank.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react"
 import {
   View,
   Image,
@@ -8,33 +8,55 @@ import {
   Button,
   Switch,
   TextInput,
-  StyleSheet,
-} from "react-native";
-import DateTimePicker from 'react-native-datepicker';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import Slider from '@react-native-community/slider';
-import { CheckBox } from 'react-native-elements';
-import {SlideMenuIcon} from '../../../navigator/slideMenuIcon';
+  StyleSheet
+} from "react-native"
+import DateTimePicker from "react-native-datepicker"
+import Icon from "react-native-vector-icons/FontAwesome"
+import Slider from "@react-native-community/slider"
+import { CheckBox } from "react-native-elements"
+import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
 
 export default class Blank extends React.Component {
-  static navigationOptions = ({navigation}) => {
+  static navigationOptions = ({ navigation }) => {
     return {
-      headerLeft: <SlideMenuIcon navigationProps={navigation} />,
-    };
-  };
-  
-  state = {};
+      headerLeft: <SlideMenuIcon navigationProps={navigation} />
+    }
+  }
+
+  state = {}
 
   render = () => (
-    <View style={styles.container}>
-      <Text>This is your new component</Text>
+    <View>
+      <View style={styles.View_2}>
+        <View style={styles.View_3} />
+        <View />
+        <View />
+        <View />
+        <View />
+        <View style={styles.View_11} />
+      </View>
+      <View>
+        <View />
+        <View />
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginHorizontal: 16,
+    marginHorizontal: 16
   },
-});
+  View_1: {},
+  View_2: { flexDirection: "row" },
+  View_3: { width: 100, height: 100 },
+  View_4: {},
+  View_8: {},
+  View_9: {},
+  View_10: {},
+  View_11: { width: 100, height: 100 },
+  View_5: {},
+  View_6: {},
+  View_7: {}
+})
